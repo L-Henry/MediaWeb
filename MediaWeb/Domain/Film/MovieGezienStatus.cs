76350d036a9accd8156deab31ebbb413593da0ff
L-Henry <Henry.Ludwig@telenet.be>
2019-09-20 10:33:52 +0200

    Add project files.

---
+++ MediaWeb/Domain/Film/MovieGezienStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaWeb.Domain
+{
+    public class MovieGezienStatus
+    {
+
+        public int Id { get; set; }
+        public string Naam { get; set; }
+
+    }
+}
---
+++ MediaWeb/Domain/Film/MovieRatingReview.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaWeb.Domain
+{
+    public class MovieRatingReview
+    {
+        public int MovieId { get; set; }
+        public IdentityUser UserId { get; set; }
+
+        public int Rating { get; set; }
+        public string Review { get; set; }
+    }
+}
